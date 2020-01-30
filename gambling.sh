a5d0b79ef6e9c35e9c55dd7cdf4288383cccbc57
PrasadPalkar <explorehorizon15@gmail.com>
2020-01-30 12:08:23 +0530

    ADDED:ResignDayCondition

--- gambling.sh
+++ gambling.sh
@@ -2,16 +2,36 @@
 echo "-----WELCOME TO GAMBLING SIMULATION---------"
 STAKE_FOR_DAY=100
 BET_PER_GAME=1
+LOSE=1
+WIN=0
+cash=$STAKE_FOR_DAY
+
+function resignpercent() {
+echo "$(($cash*50/$STAKE_FOR_DAY))"
+}
+val="$(resignpercent)"
 
 function checkWon(){
+totalval=0
 check=$((RANDOM%2))
-if [ $check -eq 1 ]
+if [ $check -eq $WIN ]
 then
 echo "WON"
-amount=$(($STAKE_FOR_DAY+$BET_PER_GAME))
+wonresign=$(($STAKE_FOR_DAY+$val))
+while [ $cash -ne $wonresign ]
+do
+cash=$(($cash+$BET_PER_GAME))
+totalval=$(($totalval+$cash))
+done
 else
 echo "LOST"
-amount=$(($STAKE_FOR_DAY-$BET_PER_GAME))
+loseresign=$(($STAKE_FOR_DAY-$val))
+while [ $cash -ne $loseresign ]
+do
+cash=$(($cash-$BET_PER_GAME))
+totalval=$(($totalval+$cash))
+done
 fi        
 }
 checkWon
+
