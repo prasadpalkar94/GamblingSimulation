891b7e90db5f02fd28143cfe7be87347a9341871
PrasadPalkar <explorehorizon15@gmail.com>
2020-01-30 15:08:32 +0530

    ADDED:Total Amount Won OR Lost

--- gambling.sh
+++ gambling.sh
@@ -5,7 +5,7 @@ BET_PER_GAME=1
 LOSE=1
 WIN=0
 cash=$STAKE_FOR_DAY
-
+DAYS_TO_PLAY=20
 function resignpercent() {
 	echo "$(($cash*50/$STAKE_FOR_DAY))"
 }
@@ -22,10 +22,25 @@ function checkWon(){
 		if [ $check -eq $WIN ]
 		then
 			cash=$(($cash+$BET_PER_GAME))	
-	else
+		else
 			cash=$(($cash-$BET_PER_GAME))
-		fi        
+			fi        
 	done
+			totalcashobtained=$(($cash-$STAKE_FOR_DAY))
+			echo $totalcashobtained
+}
+
+function wonORlost(){
+	for((i=1;i<=$DAYS_TO_PLAY;i++))
+	do
+   totalcash=$(($totalcash+$(checkWon)))
+	done
+   if [ $totalcash -gt $WONRESIGN ]
+	then
+		echo Total cash won:$totalcash
+	else
+		echo Total cash lost:$totalcash
+	fi
 }
 checkWon
-
+wonORlost
