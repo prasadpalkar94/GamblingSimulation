d7815864e3ef04506574c2ab25f03e95ffa01e8b
PrasadPalkar <explorehorizon15@gmail.com>
2020-01-30 10:41:52 +0530

    ADDED:Check Won OR Lost

--- gambling.sh
+++ gambling.sh
@@ -2,5 +2,16 @@
 echo "-----WELCOME TO GAMBLING SIMULATION---------"
 STAKE_FOR_DAY=100
 BET_PER_GAME=1
-        
 
+function checkWon(){
+check=$((RANDOM%2))
+if [ $check -eq 1 ]
+then
+echo "WON"
+amount=$(($STAKE_FOR_DAY+$BET_PER_GAME))
+else
+echo "LOST"
+amount=$(($STAKE_FOR_DAY-$BET_PER_GAME))
+fi        
+}
+checkWon
